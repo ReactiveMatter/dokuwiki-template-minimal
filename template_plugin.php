77bc0ffe8a39c3210c7e71ac61c6dddd53c6515f
ReactiveMatter <reactivematter@protonmail.com>
2024-10-14 18:43:00 +0530

    Create template_plugin.php

---
+++ template_plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Minimal Template Helper Plugin (for common tasks)
+ *
+ * @link     http://dokuwiki.org/template:minimal
+ * @author   Reactive Matter <reactivematter@protonmail.com>
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+// must be run from within DokuWiki
+if (!defined('DOKU_INC')) die();
